4b02867b1b8dfba0ac9671e161873e2429162581
Michael <michael.barrientos1129@gmail.com>
2023-08-17 17:14:18 -0400

    classification added

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,7 +21,7 @@ species = []
 for i in range(len(iris['target'])):
     if iris['target'][i] == 0:
         species.append("setosa")
-    elif iris['target'][i] ==1:
+    elif iris['target'][i] == 1:
         species.append('versicolor')
     else:
         species.append('virginica')
@@ -48,3 +50,25 @@ ax.set_title("Iris Petals")
 ax.legend()
 
 plt.show()
+
+#Performing Classifications
+
+#Dropping the target and species since only measurments ae needed
+X = iris.drop(['target', 'species'], axis = 1)
+
+#Covert into NumPy array and assign petal lenght and width
+X = X.to_numpy() [:, (2,3)]
+Y = iris['target']
+
+#Split inot train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.5, random_state = 42)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train, Y_train)
+
+#Training Predictions
+training_prediction = log_reg.predict(X_train)
+test_prediction = log_reg.predict(X_test)
+
+print(training_prediction)
+print(test_prediction)
